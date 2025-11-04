b7c735d2fdcc188ffe181f8270829a7b741e8aad
Jin <munjin1340@gmail.com>
2025-11-04 17:02:42 +0100

    feat: connect Searchfilter at Review page

--- src/app/review/page.tsx
+++ src/app/review/page.tsx
@@ -1,7 +1,28 @@
-export default function ReviewPage() {
+'use client';
+import { useState } from 'react';
+import SearchBar from '@/app/explore/components/searchBar';
+import ReviewForm from '@/app/review/ReviewForm';
+import ReviewHeader from '@/app/movies/[id]/components/ReviewHeader';
+
+export default function WriewPage() {
+  const [movie, setMovie] = useState(null); // 선택된 영화 정보
+
+  const handleMovieSelect = (selectedMovie: any) => {
+    setMovie(selectedMovie); // 영화 정보 업데이트
+  };
+
   return (
-    <div>
-      <h1>Write Review Page</h1>
-    </div>
+    <>
+      <ReviewHeader />
+      <div className="min-h-screen bg-linear-to-b from-[#0a0a0f] to-[#0d0d19] text-gray-100 px-6 pt-20 py-10 flex flex-col items-center">
+        {!movie ? (
+          // 영화가 없으면 검색창을 보여준다.
+          <SearchBar onMovieSelect={handleMovieSelect} />
+        ) : (
+          // 영화가 있으면 리뷰 폼을 보여준다.
+          <ReviewForm movie={movie} />
+        )}
+      </div>
+    </>
   );
-} 
+}
